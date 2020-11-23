afda4fab728e9b98599df4411b76fedd551c44fc
Jizga <jizga93@gmail.com>
2020-11-23 22:44:38 +0100

    Añadir nueva tarea

--- app.js
+++ app.js
@@ -5,10 +5,41 @@ const template = document.getElementById("template").content;
 const fragment = document.createDocumentFragment();
 let tareas = {};
 
-
 //Añadir la nueva tarea mediante el DOM
-
 formulario.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(input.value);
+  setTarea(e);
 });
+
+const setTarea = (e) => {
+  //No agregar tarea vacía
+  if (input.value.trim() === "") {
+    console.log("debes escribir una tarea");
+    return;
+  }
+  //Nuevas tareas con sus id
+  const tarea = {
+    id: Date.now(),
+    texto: input.value,
+    estado: false,
+  };
+
+  tareas[tarea.id] = tarea;
+  //Eliminar el texto de la nueva tarea en la zona del input
+  formulario.reset();
+  //Mantener efecto de focus en el input
+  input.focus();
+
+  pintarTareas();
+};
+
+const pintarTareas = () => {
+  //Para que la lista no se duplique debe de partir de cero (limpiar el DOM)
+  listaTarea.innerHTML = "";
+  Object.values(tareas).forEach((tarea) => {
+    const clone = template.cloneNode(true);
+    clone.querySelector("p").textContent = tarea.texto;
+    fragment.appendChild(clone);
+  });
+  listaTarea.appendChild(fragment);
+};
