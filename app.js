569a2d9fc20e219854c986bc652f374843e93db8
Jizga <jizga93@gmail.com>
2020-11-23 22:09:31 +0100

    Añadir nuevas tareas (consola)

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Incribete a la web");
+const formulario = document.getElementById("formulario");
+const input = document.getElementById("input");
+const listaTarea = document.getElementById("lista-tarea");
+const template = document.getElementById("template").content;
+const fragment = document.createDocumentFragment();
+let tareas = {};
+
+
+//Añadir la nueva tarea mediante el DOM
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(input.value);
+});
